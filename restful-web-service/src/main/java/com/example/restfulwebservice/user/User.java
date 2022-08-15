3bda3f0c4293e9f1a24569d079588b7c0e86c27a
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-08-15 22:11:15 +0900

    User services API validation
    User service API validation 추가

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class User {
     private Integer id;
-    @Size(min = 2)
+    @Size(min = 2, message = "Name은 2글자 이상 입려해주세요.")
     private String name;
     @Past
     private Date joinDate;
