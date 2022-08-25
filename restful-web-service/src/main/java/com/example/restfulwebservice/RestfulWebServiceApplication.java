6151666ac2ecffc216bcbfa549b9e5928df45491
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-08-25 23:24:54 +0900

    User services API Internationalization
    Response filtering 제어

--- restful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -17,7 +17,7 @@ public class RestfulWebServiceApplication {
 	}
 
 	@Bean
-	public LocaleResolver  localeResolver() {
+	public LocaleResolver localeResolver() {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.KOREA);
 		return localeResolver;
--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,8 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +12,15 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+//@JsonIgnoreProperties(value={"password"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name은 2글자 이상 입려해주세요.")
     private String name;
     @Past
     private Date joinDate;
+
+    private String password;
+    private String ssn;
 }
