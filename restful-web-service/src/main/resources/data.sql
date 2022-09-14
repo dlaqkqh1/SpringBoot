3837d6ed66d9ec8f96013ac449e9c89ae8812b87
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-09-14 23:53:43 +0900

    User services API POST test2
    User services API POST test2

--- restful-web-service/src/main/resources/data.sql
+++ restful-web-service/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into USER values(9001, sysdate(), 'User1', 'test1111', '701010-1111111');
 insert into USER values(9002, sysdate(), 'User2', 'test2222', '801010-2222222');
 insert into USER values(9003, sysdate(), 'User3', 'test3333', '901010-1111111');
+
+insert into post values(10001, 'My first post', 9001);
+insert into post values(10002, 'My second post', 9001);
