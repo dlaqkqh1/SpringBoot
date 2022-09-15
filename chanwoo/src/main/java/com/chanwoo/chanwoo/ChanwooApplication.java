58cec1d813be66a945c3828a9d2b36bb8d77a671
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-09-15 23:09:32 +0900

    my shopping mall project initialize
    my shopping mall project initialize

---
+++ chanwoo/src/main/java/com/chanwoo/chanwoo/ChanwooApplication.java
@@ -0,0 +1,13 @@
+package com.chanwoo.chanwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanwooApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanwooApplication.class, args);
+	}
+
+}
---
+++ chanwoo/src/test/java/com/chanwoo/chanwoo/ChanwooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanwoo.chanwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanwooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
