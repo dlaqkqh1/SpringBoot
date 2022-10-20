2d269ca096d366beb6b3c7f24f21dd8c00b7d040
dlaqkqh1 <dlaqkqh1@gmail.com>
2022-10-20 23:22:15 +0900

    add Spring REST Docs2
    Spring REST Docs 추가2

--- chanwoo/src/test/java/com/chanwoo/chanwoo/controller/ProductControllerDocTest.java
+++ chanwoo/src/test/java/com/chanwoo/chanwoo/controller/ProductControllerDocTest.java
@@ -2,40 +2,60 @@ package com.chanwoo.chanwoo.controller;
 
 import com.chanwoo.chanwoo.domain.Product;
 import com.chanwoo.chanwoo.repository.ProductRepository;
+import com.chanwoo.chanwoo.request.ProductCreate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.chanwooshop.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class ProductControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /*
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
     }
+*/
 
     @Test
     @DisplayName("움")
@@ -47,11 +67,44 @@ public class ProductControllerDocTest {
                 .build();
         productRepository.save(product);
         
-        this.mockMvc.perform(get("/products/{productId}", 1L)
-                        .accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/products/{productId}", 1L)
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andDo(document("index"));
+                .andDo(print())
+                .andDo(document("index"
+                        , pathParameters(
+                            RequestDocumentation.parameterWithName("productId").description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글 ID"),
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("글 등록")
+    void test2() throws Exception {
+        ProductCreate request = ProductCreate.builder()
+                .title("호돌맨")
+                .content("내용")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/products")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("index",
+                        requestFields(
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
     }
 
 }
